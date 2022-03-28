a1f300f58320613e96016bc311f07a7a42e8e925
estudo <gisele.nl@hotmail.com>
2022-03-28 20:25:52 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,9 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("meu primeiro programa em java");
+	}
+
+}
